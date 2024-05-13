8c03c2a415fc4c0268030bd4abedf91ad71bc83a
Alexander Nohe <nohe@Alexanders-MacBook-Pro-24.local>
2024-05-12 21:22:48 -0700

    install catpuccin colorscheme

--- init.lua
+++ init.lua
@@ -240,6 +240,9 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- Catpuccin color scheme
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
